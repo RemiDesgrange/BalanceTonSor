e5f4e230bb9c3ebe8e9f4862d8eccf70706669d6
Rémi Desgrange <remi.desgrange@fibrea.net>
2017-11-14 10:33:39 +0100

    more shit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import io
 import os
 from flask import Flask, Response, jsonify, request
 from random import randint
-import pyOTDR.read as read
+from pyOTDR import read
 
 app = Flask('BalanceTonSor')
 
