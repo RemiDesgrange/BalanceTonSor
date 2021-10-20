a87da48cff3b2286c9570401d46956c2aa98e9f3
Rémi Desgrange <remi.desgrange@camptocamp.com>
2021-10-20 22:18:22 +0200

    Revert "Modify message"
    This reverts commit ed9672d0a4c34ea823a399a86932db89f7d56ce7.

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def index_get() -> Response:
     """
     :return Response
     """
-    return jsonify({'name': 'BalTanceTonSor blah',
+    return jsonify({'name': 'BalTanceTonSor',
                     'details': 'This little api take sor file as input and return a json. Two endpoints: "/metadata" and "/data_points"'
                    })
 
